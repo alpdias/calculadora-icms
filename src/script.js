6d49d32b34a290a5a2d2ce3a39d740cf8a82907a
Paulo <al.pdias24@gmail.com>
2020-07-03 07:14:02 -0300

    add html e js elements

---
+++ src/script.js
@@ -0,0 +1,11 @@
+/*
+Criado em 07/2020
+@Autor: Paulo https://github.com/alpdias
+*/
+
+// variaveis para os elementos html
+var origem = document.querySelector('#origem');
+var origemSelect = document.querySelector('#origemSelect'); //console.log(origemSelect[2].text); tag 'select' pode ser usada com index
+var destino = document.querySelector('#destino');
+var destinoSelect = document.querySelector('#destinoSelect');
+
