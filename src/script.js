47f9b30f82c17ebc91b9692ff9f8a2014844c709
Paulo <al.pdias24@gmail.com>
2020-07-04 14:09:31 -0300

    add functions js

--- src/script.js
+++ src/script.js
@@ -8,4 +8,20 @@ var origem = document.querySelector('#origem');
 var origemSelect = document.querySelector('#origemSelect'); //console.log(origemSelect[2].text); tag 'select' pode ser usada com index
 var destino = document.querySelector('#destino');
 var destinoSelect = document.querySelector('#destinoSelect');
+var origemText = origemSelect.options[origemSelect.selectedIndex].text; // opbter o valor dentro do elemento 'option'no html
+var destinoText = destinoSelect.options[destinoSelect.selectedIndex].text;
+
+function carregarOrigem() { // funçao para mostrar no elemento 'input' o valor selecionado
+
+    var carregar = origemSelect.value;
+    origem.value = carregar;
+
+};
+
+function carregarDestino() { // funçao para mostrar no elemento 'input' o valor selecionado
+
+    var carregar = destinoSelect.value;
+    destino.value = carregar;
+
+};
 
