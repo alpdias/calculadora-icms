89e03dba20e192f9878e849f7fa43e912599881d
Paulo <al.pdias24@gmail.com>
2020-07-07 21:13:19 -0300

    add functions and update index

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -3,13 +3,10 @@ Criado em 07/2020
 @Autor: Paulo https://github.com/alpdias
 */
 
-// variaveis para os elementos html
-var origem = document.querySelector('#origem');
-var origemSelect = document.querySelector('#origemSelect'); //console.log(origemSelect[2].text); tag 'select' pode ser usada com index
-var destino = document.querySelector('#destino');
-var destinoSelect = document.querySelector('#destinoSelect');
+function carregarOrigem() { // funçao para mostrar no elemento 'input' o valor selecionado e definir o valor
 
-function carregarOrigem() { // funçao para mostrar no elemento 'input' o valor selecionado
+    var origem = document.querySelector('#origem');
+    var origemSelect = document.querySelector('#origemSelect'); //console.log(origemSelect[2].text); tag 'select' pode ser usada com index
 
     var carregar = origemSelect.value;
     origem.value = carregar;
@@ -76,9 +73,68 @@ function carregarOrigem() { // funçao para mostrar no elemento 'input' o valor 
         aliquotaOrigem = 0;
     };
 
+    return aliquotaOrigem;
+
 };
- 
+
+function calculoSimples() { // funçao para calcular o valor do ICMS simples
+
+    var base = document.querySelector('#base').value; // valor do elemento 'input' com id '#base' (valor para base de calculo)
+
+    valorOrigem = carregarOrigem(); // valor da aliquota de origem
+
+};
+
+function verificarCompleto() { // funçao para verificar se o elemento 'radio' no html esta preenchido e inserir html
+
+    document.querySelector('#interestadual').checked = true; // verificar 'radio' checked
+
+    document.querySelector('#completo').innerHTML = '<input type="text" name="destino" id="destino" placeholder="Estado Destino">\
+        <select name="destinoSelect" id="destinoSelect" onclick="carregarDestino()">\
+        <option value=""></option>\
+        <option value="Acre">AC</option>\
+        <option value="Alagoas">AL</option>\
+        <option value="Amapá">AP</option>\
+        <option value="Amazonas">AM</option>\
+        <option value="Bahia">BA</option>\
+        <option value="Ceará">CE</option>\
+        <option value="Distrito Federal">DF</option>\
+        <option value="Espírito Santo">ES</option>\
+        <option value="Goiás">GO</option>\
+        <option value="Maranhão">MA</option>\
+        <option value="Mato Grosso">MT</option>\
+        <option value="Mato Grosso do Sul">MS</option>\
+        <option value="Minas Gerais">MG</option>\
+        <option value="Pará">PA</option>\
+        <option value="Paraíba">PB</option>\
+        <option value="Paraná">PR</option>\
+        <option value="Pernambuco">PE</option>\
+        <option value="Piauí">PI</option>\
+        <option value="Rio de Janeiro">RJ</option>\
+        <option value="Rio Grande do Norte">RN</option>\
+        <option value="Rio Grande do Sul">RS</option>\
+        <option value="Rondônia">RO</option>\
+        <option value="Roraima">RR</option>\
+        <option value="Santa Catarina">SC</option>\
+        <option value="São Paulo">SP</option>\
+        <option value="Sergipe">SE</option>\
+        <option value="Tocantins">TO</option>\
+        </select>';
+
+};
+
+function verificarSimples() { // funçao para verificar se o elemento 'radio' no html esta preenchido e inserir um html novo
+
+    document.querySelector('#simples').checked = true; // verificar 'radio' checked
+
+    document.querySelector('#completo').innerHTML = ''; // inserir html 
+    
+};
+
 function carregarDestino() { // funçao para mostrar no elemento 'input' o valor selecionado
+
+    var destino = document.querySelector('#destino');
+    var destinoSelect = document.querySelector('#destinoSelect'); //console.log(origemSelect[2].text); tag 'select' pode ser usada com index
 
     var carregar = destinoSelect.value;
     destino.value = carregar;
@@ -144,5 +200,17 @@ function carregarDestino() { // funçao para mostrar no elemento 'input' o valor
     } else {
         aliquotaDestino = 0;
     };
+
+    return aliquotaDestino;
+
+};
+
+function calculoCompleto() {
+
+    var base = document.querySelector('#base').value; // valor do elemento 'input' com id '#base' (valor para base de calculo)
+
+    valorOrigem = carregarOrigem(); // valor da aliquota de origem
+
+    valorDestino = carregarDestino(); // valor da aliquota de destino
 
 };
